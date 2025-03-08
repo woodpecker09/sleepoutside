e10e7a237180307bab7e1c980175d4fc8a276cf5
woodpecker09 <ortegaharold99@outlook.es>
2025-03-07 22:39:51 -0700

    final decition Add Item to Cart

--- src/js/product.js
+++ src/js/product.js
@@ -5,7 +5,7 @@ const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product); 
+  cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
