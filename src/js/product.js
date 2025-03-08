bbc52ef58c707cddb9f687b678e829cb321ab6af
woodpecker09 <ortegaharold99@outlook.es>
2025-03-07 22:24:32 -0700

    Add Item to Cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,9 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product); 
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
