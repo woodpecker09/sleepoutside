435bca6b9591698a4284a3f0cdebb56d45490d9c
woodpecker09 <ortegaharold99@outlook.es>
2025-03-07 22:36:32 -0700

    new update

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,10 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  const cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product); 
   setLocalStorage("so-cart", cartItems);
 }
