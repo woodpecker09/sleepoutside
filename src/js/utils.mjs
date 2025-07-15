8d2e0d0213660a5353207e526614d2b7604cd364
woodpecker09 <ortegaharold99@outlook.es>
2025-07-14 21:38:23 -0600

    W02 Individual Activity: Dynamic Product List Done

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -29,3 +29,11 @@ export function getParam(param){
   const product = urlParams.get(param);
   return product ? product : null;
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list,position = "afterbegin", clear = false){
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
