a439a1e1b50ed47d6a779be9352457d249594fe0
woodpecker09 <ortegaharold99@outlook.es>
2025-07-23 19:21:18 -0600

    team activity week03 done

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -30,10 +30,35 @@ export function getParam(param){
   return product ? product : null;
 }
 
-export function renderListWithTemplate(templateFn, parentElement, list,position = "afterbegin", clear = false){
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false){
   const htmlStrings = list.map(templateFn);
   if (clear) {
     parentElement.innerHTML = "";
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if(callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter() {
+  
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
