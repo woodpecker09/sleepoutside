6731e4a0b3dc27483f932143f384c2e48c79b406
woodpecker09 <ortegaharold99@outlook.es>
2025-08-10 11:34:48 -0600

    W04 Team Activity: Checkout Done

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,19 @@
 import { loadHeaderFooter } from "./utils.mjs"; 
+import  CheckoutProcess  from "./CheckoutProcess.mjs";
 
 loadHeaderFooter();
+
+const order = new CheckoutProcess("so-cart", ".checkout-summary");
+order.init();
+
+// Add event listeners to fire calculateOrderTotal when the user changes the zip code
+document
+  .querySelector("#zip")
+  .addEventListener("blur", order.calculateOrderTotal.bind(order));
+
+// listening for click on the button
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  order.checkout();
+});
--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,9 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 loadHeaderFooter();
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 const productId = getParam("products");
 const productDetails = new ProductDetails(productId, dataSource);
 productDetails.init();
